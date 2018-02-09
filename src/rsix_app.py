adbaa1e7e571bd59730a54a1165e03227b9d40f2
Lucas Arbiza <lucas@pop-rs.rnp.br>
2018-02-09 14:06:40 -0200

    Added license and more comments. The original file comes from the Ryu project, so I added the same Apache license. I also informed it was (will be for now) modified into a fully functional learning switch.

--- src/rsix_app.py
+++ src/rsix_app.py
@@ -1,6 +1,31 @@
-# Code and explanations from
-# https://osrg.github.io/ryu-book/en/html/switching_hub.html
-# http://ryu.readthedocs.io/en/latest/api_ref.html
+# Copyright (C) 2018 Lucas Arbiza - [SD]RSiX Project
+#               2016 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ------------------------------------------------------------------------------
+#
+# This file started as a fully commented version of the ExampleSwitch13.py App
+# from the Ryu project
+# (https://github.com/osrg/ryu/blob/master/ryu/app/example_switch_13.py), but
+# the original version has been modified to make it in a multi-switches learning
+# app (the original version does not work with more than one switch).
+#
+# Code and explanations from:
+#  - https://osrg.github.io/ryu-book/en/html/switching_hub.html
+#  - http://ryu.readthedocs.io/en/latest/api_ref.html
+#
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
