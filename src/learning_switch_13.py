610cdc3caddfe548e9932f689da37b5626e87fe9
Lucas Arbiza <lucas@pop-rs.rnp.br>
2018-02-26 16:07:54 -0300

    Learning switch updated to fully working version.
    The previous version was not working properly, because hosts connected to a
    switch couldn't ping hosts connected to other switches. The version from the
    link below worked well. The code was merged to get a well documented and working
    version.
    
    https://github.com/osrg/ryu/blob/master/ryu/app/simple_switch_13.py

--- src/learning_switch_13.py
+++ src/learning_switch_13.py
@@ -18,19 +18,18 @@
 #
 # This file started as a fully commented version of the ExampleSwitch13.py App
 # from the Ryu project
-# (https://github.com/osrg/ryu/blob/master/ryu/app/example_switch_13.py), but
+# (https://github.com/osrg/ryu/blob/master/ryu/app/simple_switch_13.py), but
 # the original version has been modified to make it in a multi-switches learning
 # app (the original version does not work with more than one switch).
-#
-
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3, ofproto_v1_4, ofproto_v1_5
+from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
 
 
 class LearningSwitch(app_manager.RyuApp):
@@ -42,10 +41,7 @@ class LearningSwitch(app_manager.RyuApp):
 
     # A list of supported OpenFlow versions for this RyuApp. The default is all
     # versions supported by the framework.
-    OFP_VERSIONS = [
-        ofproto_v1_3.OFP_VERSION, ofproto_v1_4.OFP_VERSION,
-        ofproto_v1_5.OFP_VERSION
-    ]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(LearningSwitch, self).__init__(*args, **kwargs)
@@ -97,15 +93,13 @@ class LearningSwitch(app_manager.RyuApp):
         # create an instance of OFPActionOutput class setting controller as the
         # output port and NO_BUFFER, so that the switch will not buffer the
         # the packets that generate a PacketIn
-        actions = [
-            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                   ofproto.OFPCML_NO_BUFFER)
-        ]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
 
-        # PacketOut
+        # Install the flow-entry
         self.add_flow(datapath, 0, match, actions)
 
-    def add_flow(self, datapath, priority, match, actions):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         """PacketOut - Install a flow entry on a switch
 
         Args:
@@ -124,17 +118,17 @@ class LearningSwitch(app_manager.RyuApp):
         # the processing.
         # Using "ofproto.OFPIT_APPLY_ACTIONS" parameter, the controller informs
         # the switch to run the action list in that very same processing stage.
-        inst = [
-            parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)
-        ]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
 
         # Create the flow mod message
-        mod = parser.OFPFlowMod(
-            datapath=datapath,
-            priority=priority,
-            match=match,
-            instructions=inst)
-
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -148,10 +142,31 @@ class LearningSwitch(app_manager.RyuApp):
             ev (ev): instance of the OpenFlow event handler class
         """
 
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+
+        # Get the in_port from the ev.msg object.
+        # Options available for OF 1.3 at https://goo.gl/qwxpaU
+        in_port = msg.match['in_port']
+
+        # Analyse the received packets using the packet library
+        # Extract MAC addresses (src, dst)
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
 
         # Get Datapach ID to identify OpenFlow switches.
         # The setdefault() dictionary method returns the value of the key (dpid)
@@ -160,18 +175,7 @@ class LearningSwitch(app_manager.RyuApp):
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        # Analyse the received packets using the packet library
-        # Extract MAC addresses (src, dst)
-        pkt = packet.Packet(msg.data)
-        eth_pkt = pkt.get_protocol(ethernet.ethernet)
-        dst = eth_pkt.dst
-        src = eth_pkt.src
-
-        # Get the in_port from the ev.msg object.
-        # Options available for OF 1.3 at https://goo.gl/qwxpaU
-        in_port = msg.match['in_port']
-
-        # print
+        # Log
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # Learn the MAC to avoid flooding next time
@@ -194,20 +198,25 @@ class LearningSwitch(app_manager.RyuApp):
         # If controller knows the destination MAC, it installs a flow entry to
         # process new packets with the same source and destination MAC.
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            self.add_flow(datapath, 10, match, actions)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
 
         # PacketOut
         # Now the controller process the packet that generated the PacketIn
         # through a PacketOut message. If the destination MAC is known, the
         # switch sends the packet through the port where the destination MAC
         # comes; else the switch floods the packet to all ports.
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=ofproto.OFP_NO_BUFFER,
-            in_port=in_port,
-            actions=actions,
-            data=msg.data)
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
 
         # send packet out
         datapath.send_msg(out)
