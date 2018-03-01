938d0934254bdb9866158a7dae5fd5237e7b2a14
Lucas Arbiza <lucas@pop-rs.rnp.br>
2018-03-01 17:33:07 -0300

    Started working on class IXP and file neighbor.py

--- src/sd_ixp/ofswitch.py
+++ src/sd_ixp/ofswitch.py
@@ -12,15 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#from ryu.base import app_manager
 from ryu.controller import ofp_event
-#from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-#from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
-#from ryu.lib.packet import packet
-#from ryu.lib.packet import ethernet
-#from ryu.lib.packet import ether_types
 
 
 class Switch:
@@ -118,9 +112,6 @@ class Switch:
                 instructions=instructions)
 
         self._datapath.send_msg(mod)
-
-    def del_flow(self, arg):
-        pass
 
     def get_flow_entries(self):
         pass
