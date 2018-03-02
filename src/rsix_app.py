9cac60cfd095ae42354dee2c3970db452ffad93b
Lucas Arbiza <lucas@pop-rs.rnp.br>
2018-03-02 17:39:43 -0300

    Learning how to parse packet_in packets.

--- src/rsix_app.py
+++ src/rsix_app.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -22,8 +21,13 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from ryu.lib.packet import icmpv6
 
 from sd_ixp.ofswitch import Switch
+from sd_ixp import neighbor
+
+import array
+
 
 class SD_RSiX(app_manager.RyuApp):
     """OpenFlow learning switch
@@ -44,7 +48,6 @@ class SD_RSiX(app_manager.RyuApp):
         #       { datapath_id, Switch }
         self.switches = {}
 
-
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_up(self, ev):
         """Install table-miss flow entry
@@ -58,7 +61,6 @@ class SD_RSiX(app_manager.RyuApp):
         """
 
         self.switches[ev.msg.datapath.id] = Switch(ev.msg.datapath)
-
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -77,90 +79,29 @@ class SD_RSiX(app_manager.RyuApp):
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
 
-        msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        # Get the in_port from the ev.msg object.
-        # Options available for OF 1.3 at https://goo.gl/qwxpaU
-        in_port = msg.match['in_port']
-
-        # Analyse the received packets using the packet library
-        # Extract MAC addresses (src, dst)
-        pkt = packet.Packet(msg.data)
+        pkt = packet.Packet(ev.msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        # Ignore lldp packets
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            self.logger.info("ARP")
+        elif eth.ethertype == ether_types.ETH_TYPE_IPV6:
+            for p in pkt.protocols:
+                if p.protocol_name == 'icmpv6':# and 'type_=135' in p:
+                    self.logger.info(p)
 
-        dst_MAC = eth.dst
-        src_MAC = eth.src
+            #icpmv6_pkt = pkt.get_protocols(icmpv6.icmpv6)
+            # for p in pkt.protocols:
+            #     #self.logger.info("Name: {}; type: {}".format(
+            #     #    p.protocol_name, p.get_packet_type))
+            #     if p.protocol_name == 'icmpv6':
+            #         icpmv6_pkt = pkt.get_protocols(icmpv6.icmpv6)
+            #         for algo in icpmv6_pkt:
+            #             self.logger.info("opa: {}".format(algo))
 
-        # Get Datapath ID to identify OpenFlow switches.
-        # The setdefault() dictionary method returns the value of the key (dpid)
-        # if it exists. If the the key does not exist, it adds the key (dpid)
-        # with an empty dictionary as value.
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
 
-        # Log
-        self.logger.info(
-            "PacketIn:\n\tSwitch: {0:16d}\n\tSrc: {1}; Dest: {2}\n\tIn port: {3}".
-            format(dpid, src_MAC, dst_MAC, in_port))
 
-        # Learn the MAC to avoid flooding next time
-        # The next line associates a source MAC (src_MAC) to a port (in_port)
-        # for the switch the PacketIn came from.
-        self.mac_to_port[dpid][src_MAC] = in_port
 
-        # If the controller already has the destination MAC address in its table
-        # (mac_to_port dictionary), it takes the out port from there. If not, it
-        # sets all ports as the out port.
-        if dst_MAC in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst_MAC]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-        # Create the action list. Set out port as the only action for matching
-        # packets.
-        actions = [parser.OFPActionOutput(out_port)]
-
-        # If controller knows the destination MAC, it installs a flow entry to
-        # process new packets with the same source and destination MAC.
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(
-                in_port=in_port, eth_dst=dst_MAC, eth_src=src_MAC)
-
-            # Check if buffer_id is valid.
-            # If the message has a valid buffer_id, it means the switch kept the
-            # original packet so that controller does not need to send a
-            # PacketOut (function returns after installing the flow_mod)
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            # If there is no a valid buffer_id, the switch does not have the
-            # original packet, and the controller must send a PakectOut in order
-            # the switch can forward the original packet sent to the controller.
-            else:
-                self.add_flow(datapath, 1, match, actions)
-
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        # PacketOut
-        # Now the controller process the packet that generated the PacketIn
-        # through a PacketOut message. If the destination MAC is known, the
-        # switch sends the packet through the port where the destination MAC
-        # comes; else the switch floods the packet to all ports.
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=msg.buffer_id,
-            in_port=in_port,
-            actions=actions,
-            data=data)
-
-        # send packet out
-        datapath.send_msg(out)
+        # pkt = packet.Packet(array.array('B', ev.msg.data))
+        # for p in pkt.protocols:
+        #     print(p)
+        # print('-' * 80)
--- src/sd_ixp/neighbor.py
+++ src/sd_ixp/neighbor.py
@@ -12,10 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-def discovery_handler(l2_packet):
-    """ 
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+
+def discovery_handler(eth_pkt):
     """
-    pass
+
+    Args:
+        pkt (packet.Packet(msg.data)): packet object containing layer 2 data
+    """
+
+    # ICMPv6
+    #
+    # ethernet(dst='33:33:ff:00:00:01', ethertype=34525, src='00:50:79:66:68:01')
+    # ipv6(
+    #     dst='ff02::1:ff00:1',
+    #     ext_hdrs=[],
+    #     flow_label=0,
+    #     hop_limit=255,
+    #     nxt=58,
+    #     payload_length=32,
+    #     src='2001:db8::4',
+    #     traffic_class=0,
+    #     version=6)
+    # icmpv6(
+    #     code=0,
+    #     csum=15471,
+    #     data=nd_neighbor(
+    #         dst='2001:db8::1',
+    #         option=nd_option_sla(
+    #             data=None, hw_src='00:50:79:66:68:01', length=1),
+    #         res=0),
+    #     type_=135)
+
+
+    # print('-' * 80)
+    #
+    # if eth_pkt.ethertype == ether_types.ETH_TYPE_ARP:
+    #     print("ARP")
+    # elif eth_pkt.ethertype == ether_types.ETH_TYPE_IPV6:
+    #
+    #
+    #
+    # print('-' * 80)
+
 
 def lldp_handler(llpd_packet):
     pass
